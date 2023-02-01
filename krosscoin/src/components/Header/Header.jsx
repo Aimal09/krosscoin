00f994f646fcf08294d209e47b555e3bc7e48bdd
decryptus007 <domnick080@outlook.com>
2023-02-01 17:31:08 +0100

    navlink bold

--- krosscoin/src/components/Header/Header.jsx
+++ krosscoin/src/components/Header/Header.jsx
@@ -7,13 +7,13 @@ function Header() {
     <div className='bg-primary px-2 py-4 h-[100px]'>
       <div className="flex items-center h-full gap-[108px] 3xl:container 3xl:mx-auto">
         <img src={logo} alt="kross-logo" />
-        <NavLink to={""} className="text-white">
+        <NavLink to={""} className="text-white font-bold">
           Home
         </NavLink>
-        <NavLink to={""} className="text-white">
+        <NavLink to={""} className="text-white font-bold">
           Products
         </NavLink>
-        <NavLink to={""} className="text-white">
+        <NavLink to={""} className="text-white font-bold">
           Services
         </NavLink>
       </div>
