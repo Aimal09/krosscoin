06a28f7eb04589af46026a1128c0a41b69cf0f7a
decryptus007 <domnick080@outlook.com>
2023-02-01 17:29:53 +0100

    header complete

---
+++ krosscoin/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../../assets/images/kross-header-logo.png'
+
+function Header() {
+  return (
+    <div className='bg-primary px-2 py-4 h-[100px]'>
+      <div className="flex items-center h-full gap-[108px] 3xl:container 3xl:mx-auto">
+        <img src={logo} alt="kross-logo" />
+        <NavLink to={""} className="text-white">
+          Home
+        </NavLink>
+        <NavLink to={""} className="text-white">
+          Products
+        </NavLink>
+        <NavLink to={""} className="text-white">
+          Services
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- krosscoin/tailwind.config.js
+++ krosscoin/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1600px',
+      },
+      colors: {
+        primary: "#050026",
+      },
+    },
   },
   plugins: [],
 }
